82c67ab122ad93af9ad3b680601715e9cbd8eb42
youssefbourais0310 <youssef.bourais0310@gmail.com>
2024-03-07 18:55:09 +0100

    sonthing

---
+++ mmap.cpp
@@ -0,0 +1,24 @@
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd = open("/Users/ybourais/Desktop/blanat/mini.txt", O_RDWR);
+
+    struct stat sb;
+
+    int fs;
+    fs = fstat(fd, &sb);
+    char *containt = (char *)mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    int i = 0;
+    while (i < sb.st_size) 
+    {
+        write(1, &containt[i++], 1);
+    }
+    munmap(containt, sb.st_size);
+    close(fd);
+}
--- server.cpp
+++ server.cpp
@@ -1,67 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.cpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 20:02:50 by ybourais          #+#    #+#             */
-/*   Updated: 2024/01/28 00:04:14 by ybourais         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-
-#include <iostream>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <sys/_types.h> // for data types used in socket-related functions
+#include <iostream>
+#include <netinet/in.h> // for IPv4 address-related constants and structures
 
-int main() {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        perror("Error creating socket");
+int main()
+{
+    int sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    if(sockfd == -1)
+    {
+        std::cerr<< "failed to open socket"<<std::endl;
         return 1;
     }
-
-    struct sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(10);
-
-    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        perror("Error binding socket");
-        close(serverSocket);
+    struct sockaddr_in address;
+    address.sin_family = AF_INET; // indicates the type of address the socket will use
+    address.sin_port = htons(8080);
+    address.sin_addr.s_addr = INADDR_ANY; // any availabe address
+ 
+    if(bind(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1)
+    {
+        std::cerr<< "failed to bind socket to port"<<std::endl;
         return 1;
     }
-
-    if (listen(serverSocket, 1) == -1) {
-        perror("Error listening for connections");
-        close(serverSocket);
+    if(listen(sockfd, 3) == -1)
+    {
+        std::cerr<< "failed to puts the server socket in a passive mode"<<std::endl;
         return 1;
     }
+    socklen_t addr_length = sizeof(address);
+    int new_socket = accept(sockfd, (struct sockaddr *)&address, &addr_length);   
 
-    std::cout << "Waiting for a connection..." << std::endl;
 
-    while (1) {
-        struct sockaddr_in clientAddress;
-        socklen_t clientAddressLength = sizeof(clientAddress);
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
-        if (clientSocket == -1) {
-            perror("Error accepting connection");
-            close(serverSocket);
-            return 1;
-        }
-
-        std::cout << "A response is coming in!" << std::endl;
-
-        // Perform your operations with the connected client here
-
-        close(clientSocket);
-    }
-
-    close(serverSocket);
-
-    return 0;
+    std::cout << sockfd<<std::endl;
 }
+
+
 
